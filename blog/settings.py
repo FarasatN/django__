437812402beebb690ba65a9e935c76d806ae0358
Farasat <ferasetnovruzov1@gmail.com>
2020-10-13 15:35:13 +0400

    ok

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@2jg(8x*vts4o_@&33@dx1m=ztef=%nuyqffvl_2fj^r5c+9z1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
